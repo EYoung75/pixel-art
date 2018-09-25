b2ce7d9b9bcc5c2b1ea0f208eb762d3ebeee82c0
EYoung75 <eyoung75.ey@gmail.com>
2018-09-25 15:53:57 -0600

    reformatted styling and removed broken current color indicator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var gridSize = prompt("Select grid size 'small', 'medium' or 'large'");
 var canvas = document.getElementById("canvas");
 
+
 if (gridSize === "small") {
     for (small = 0; small < 64; small++) {
         var smallCell = document.createElement("div")
@@ -25,23 +26,22 @@ if (gridSize === "small") {
         canvas.appendChild(largeCell)
         canvas.classList.add("canvasLg")
     }
-}
+};
 
-var palette = document.getElementById("palette")
-canvas.addEventListener("mouseover", pointer)
-palette.addEventListener("mouseover", pointer)
 
 function pointer() {
     canvas.style.cursor = "pointer";
     palette.style.cursor = "pointer";
 
-}
+};
+
+var palette = document.getElementById("palette");
 
 var colors = ["#3268E8", "#881D36", "#8F3039", "#838795", "#29335C",
     "#553D36", "#808782", "#6A7062", "#87A0B2", "#D5C7BC", "#FB8B24", "#FDE74C", "#F3A712", "#CE8147", "#E4572E",
     "#1C7C54", "#29BF12", "#A2D729", "#19381F", "#A1E44D", "#D3BDB0", "#CB8589", "#D90368", "#414288", "#682D63",
     "#FFFFFF", "#000000", "#272932", "#B4E1FF", "#9D5C63", "#FFACE4", "#D4AFB9"
-]
+];
 
 for (var i = 0; i < colors.length; i++) {
     var color = document.createElement("div")
@@ -51,12 +51,17 @@ for (var i = 0; i < colors.length; i++) {
     palette.appendChild(color)
 };
 
+canvas.addEventListener("mouseover", pointer);
+palette.addEventListener("mouseover", pointer);
+
 function fill() {
     event.target.style.backgroundColor = pen;
-}
+};
 
 var pen = color.style.backgroundColor;
 
 function chooseColor() {
     pen = event.target.style.backgroundColor;
-}
+};
+
+
